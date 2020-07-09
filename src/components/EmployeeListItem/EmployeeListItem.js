d2908eefa8f367e99321e0dd7f8c0ff6776839fb
Bin Hu <binhu10@gmail.com>
2020-07-09 14:52:59 -0400

    Update EmployeeListItem.js

--- src/components/EmployeeListItem/EmployeeListItem.js
+++ src/components/EmployeeListItem/EmployeeListItem.js
@@ -7,11 +7,9 @@ class EmployeeListItem extends React.Component {
     }
 
     render() {
-        const {name, info} = this.props.employee
-        const image = "images/" + name + ".jpg"
+        const {name, info} = this.props.employee        
         return (
-            <div className="employee-item" sstyle = {this.props.style} onClick={this.onClick}>
-                <img src={image}></img>
+            <div className="employee-item" sstyle = {this.props.style} onClick={this.onClick}>                
                 <div className="employee-item-text">
                     <h2>{name}</h2>
                     <p>{info}</p>
